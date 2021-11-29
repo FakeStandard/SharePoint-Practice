f5ca241d611fb4b0146d1e0bd925a379e70f0487
FakeStandard <rileylin.space@gmail.com>
2021-11-29 11:57:57 +0800

    add library sample

--- 02-Library/Program.cs
+++ 02-Library/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Microsoft.SharePoint;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.SharePoint;
 
-namespace _01_List
+namespace _02_Library
 {
     internal class Program
     {
@@ -17,27 +17,24 @@ namespace _01_List
                 // Site
                 using (SPWeb web = site.OpenWeb())
                 {
-                    // List
-                    SPList list = web.Lists["My Contacts"];
+                    // Library
+                    SPDocumentLibrary library = web.Lists["My Docs"] as SPDocumentLibrary;
 
                     int i = 1;
-                    foreach (SPListItem item in list.Items)
+                    foreach (SPListItem item in library.Items)
                     {
                         Console.WriteLine($"Item: {(i++).ToString()}");
 
-                        Console.WriteLine(item.Name);
-                        Console.WriteLine(item.Title);
-
-                        Console.WriteLine(item.GetFormattedValue("FirstName"));
-                        Console.WriteLine(item.GetFormattedValue("LastName"));
+                        Console.WriteLine(item.File.Name);
+                        Console.WriteLine(item.File.Length);
 
                         Console.WriteLine(String.Empty);
-
                     }
                 }
             }
 
             Console.ReadKey(false);
+
         }
     }
 }
