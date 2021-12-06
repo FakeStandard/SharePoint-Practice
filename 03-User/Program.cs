aa6a603a84f95d895b59054ea249d37a385c5a87
FakeStandard <rileylin.space@gmail.com>
2021-12-06 13:55:46 +0800

    add 03-User project and delete obj & bin

---
+++ 03-User/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace _03_User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SPSite site = new SPSite("http://sp-server-2016/"))
+            {
+                using (SPWeb web = site.OpenWeb())
+                {
+                    foreach (SPUser user in web.AllUsers)
+                        Console.WriteLine($"User: {user.Name}");
+                }
+            }
+
+            Console.ReadKey(false);
+        }
+    }
+}
